82021ea9f943819f6a95d1cb031ce65d85ba3ff0
Patryk Przekwas <patryk.przekwas@intel.com>
2017-06-24 09:59:26 +0200

    HTTP request module finished

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -19,6 +19,16 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function (req, res) {
+  console.log('returning response...');
+  res.json([
+    {"id":21569233,"firstName":"Willa","lastName":"Schmitt","email":"Damian_Kihn@yahoo.com"},
+    {"id":70415156,"firstName":"Eliane","lastName":"Shanahan","email":"Forrest_Wiza@gmail.com"},
+    {"id":59996121,"firstName":"Dashawn","lastName":"Welch","email":"Darius_Durgan76@yahoo.com"},
+    {"id":99332571,"firstName":"Trevion","lastName":"Hartmann","email":"Caroline93@hotmail.com"}
+  ]);
+});
+
 app.listen(port, function (err) {
   if (err) {
     console.log(err);
